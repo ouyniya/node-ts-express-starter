2aa99ffb1a3ee30fa54007fadd329e5b6101d075
niya-s <niya.somkerd@gmail.com>
2025-08-16 13:47:05 +0700

    start project

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express, { type Request, type Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running OK!!!");
+});
+
+export default app;
